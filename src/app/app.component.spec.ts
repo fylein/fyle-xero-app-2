be1c4ccc93b31f5ec358f30d10288e9bb3676d0c
Fyle <fyle@Fyles-MacBook-Air.local>
2022-08-12 15:29:28 +0530

    Fix Readme with local setup and other relevant information commit 3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
+// import { SharedModule } from './shared/shared.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        SharedModule
+        // SharedModule
       ],
       declarations: [
         AppComponent
