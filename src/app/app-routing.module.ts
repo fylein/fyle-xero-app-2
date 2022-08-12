0ab25bd7708e695374b22dcc00b5bdb0d40f4f1d
Fyle <fyle@Fyles-MacBook-Air.local>
2022-08-12 15:59:26 +0530

    Fix Readme with local setup and other relevant information commit 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,35 +1,35 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './core/guard/auth.guard';
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { AuthGuard } from './core/guard/auth.guard';
 
-const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/workspaces',
-    pathMatch: 'full'
-  },
-  {
-    path: 'workspaces',
-    loadChildren: () => import('./integration/integration.module').then(m => m.IntegrationModule),
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
-  },
-  {
-    path: 'xero_callback',
-    loadChildren: () => import('./xero-callback/xero-callback.module').then(m => m.XeroCallbackModule)
-  },
-  {
-    path: '**',
-    redirectTo: 'workspaces',
-    pathMatch: 'full'
-  }
-];
+// const routes: Routes = [
+//   {
+//     path: '',
+//     redirectTo: '/workspaces',
+//     pathMatch: 'full'
+//   },
+//   {
+//     path: 'workspaces',
+//     loadChildren: () => import('./integration/integration.module').then(m => m.IntegrationModule),
+//     canActivate: [AuthGuard]
+//   },
+//   {
+//     path: 'auth',
+//     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+//   },
+//   {
+//     path: 'xero_callback',
+//     loadChildren: () => import('./xero-callback/xero-callback.module').then(m => m.XeroCallbackModule)
+//   },
+//   {
+//     path: '**',
+//     redirectTo: 'workspaces',
+//     pathMatch: 'full'
+//   }
+// ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
