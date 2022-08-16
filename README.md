340efdc939d4cde64c8115f2e3f0358605c82e9f
Dhaarani <55541808+DhaaraniCIT@users.noreply.github.com>
2022-08-16 12:18:38 +0530

    Fix Readme with local setup and other relevant information (#1)
    Fix Readme with local setup and other relevant information commit

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# fyle-xero-app-2
+# fyle-xero-app
+Frontend Repository for Fyle &lt;> Xero Integration
+
+### Setup
+
+* Download and install Docker desktop for Mac from [here.](https://www.docker.com/products/docker-desktop)
+
+* If you're using a linux machine, please download docker according to the distrubution you're on.
+
+* Navigate into the root folder
+
+* Copy docker-compose.template.yml as docker-compose.yml and add environment variables wherever needed
+
+    ```
+    cp docker-compose.template.yml docker-compose.yml
+    ```
+
+* Build docker images
+
+    ```
+    docker-compose build
+    ```
+
+* Run docker containers
+
+    ```
+    docker-compose up -d app
+    ```
+
+* To tail the logs a service you can do
+
+    ```
+    docker-compose logs -f app
+    ```
+
+* To stop the containers
+
+    ```
+    docker-compose stop app
+    ```
+
+* To restart any containers -
+
+    ```
+    docker-compose restart app
+    ```
+
+* To run bash inside any container for purpose of debugging or for creating new components, services etc
+
+    ```
+    docker-compose exec app /bin/bash
+    ```
---
+++ src/app/app.module.ts
@@ -0,0 +1,60 @@
+import { ErrorHandler, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
+import { MatSnackBarModule, MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
+
+import * as Sentry from '@sentry/angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+// Import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
+import { GlobalErrorHandler } from './app.errorhandling';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSnackBarModule
+  ],
+  providers: [
+    {
+      provide: JWT_OPTIONS,
+      useValue: JWT_OPTIONS
+    },
+    JwtHelperService,
+    // {
+    //   Provide: HTTP_INTERCEPTORS,
+    //   UseClass: JwtInterceptor,
+    //   Multi: true
+    // },
+    {
+      provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {
+        duration: 2500,
+        horizontalPosition: 'end',
+        verticalPosition: 'top'
+      }
+    },
+    {
+      provide: ErrorHandler,
+      useClass: GlobalErrorHandler
+    },
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler({
+        showDialog: false
+      })
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
