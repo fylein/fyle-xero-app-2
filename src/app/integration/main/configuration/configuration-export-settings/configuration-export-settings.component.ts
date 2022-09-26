449c451c0922f6c9da2903baf2147dde4d23eb95
Fyle <fyle@Fyles-MacBook-Air.local>
2022-09-26 13:38:53 +0530

    lastest master commit

---
+++ src/app/integration/main/configuration/configuration-export-settings/configuration-export-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-configuration-export-settings',
+  templateUrl: './configuration-export-settings.component.html',
+  styleUrls: ['./configuration-export-settings.component.scss']
+})
+export class ConfigurationExportSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
