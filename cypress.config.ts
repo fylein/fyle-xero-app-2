c529207dbb3b951f9dec9e85d02c02f9a131f5a6
anishfyle <anishkumar.s@fyle.in>
2022-12-09 17:35:39 +0530

    projectID

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import environment from 'src/environments/environment.json';
 
 export default defineConfig({
   e2e: {
-    projectId: 'projectID',
+    projectId: '1umbvb',
     baseUrl: environment.app_url,
   },
   defaultCommandTimeout: environment.e2e_tests.env !== 'staging' ? 15000 : 30000,
