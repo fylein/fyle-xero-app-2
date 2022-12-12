f8237e329d10b334103606c9bba5b976c7899df8
anishfyle <anishkumar.s@fyle.in>
2022-12-13 00:25:32 +0530

    Login e2e test

---
+++ cypress/e2e/micro-actions/01-auth-login.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe('auth login', () => {
+    beforeEach(() => {
+      cy.visit('/')
+    }) 
+  
+    it('loads xero app', () => {
+      cy.url().should('include', '/auth/login')
+      cy.getElement('fyle-logo')
+      cy.assertText('login-header',"Fyle Xero Integration")
+      cy.assertText('signin-button', "Signin With Fyle").click()
+    })
+  })
+  
