19c3b06536189f583c3b7ddd6edfb192c2b7df7d
anishfyle <anishkumar.s@fyle.in>
2022-12-16 16:50:00 +0530

    Assertions -1

--- cypress/e2e/micro-actions/employee-mapping.cy.js
+++ cypress/e2e/micro-actions/employee-mapping.cy.js
@@ -9,7 +9,11 @@ describe('employee mapping', () => {
     it('in xero-app : check employee mapping', () => {
         cy.getElement('side-nav-bar')
         cy.assertText('mappings-text','Mappings')
-        cy.getElement('side-nav-bar-click').click({multiple:true})
+        cy.getElement('side-nav-bar-click').contains('Mappings').click()
+        cy.getElement('employee-mapping-text').contains('Employee Mapping').click()
+        // cy.get('.side-nav-bar--module-block-text-inner').within(() => {
+        //     cy.get('.p-ripple').eq(0).contains('20 Items').click()
+        //   })
     })
   })
   
