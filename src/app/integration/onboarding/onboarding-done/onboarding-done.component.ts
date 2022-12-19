1b0f0596733a7420fda6f299adeb94476a965c37
Fyle <fyle@Fyles-MacBook-Air.local>
2022-12-19 13:38:28 +0530

    servey placed in advanced setting page

--- src/app/integration/onboarding/onboarding-done/onboarding-done.component.ts
+++ src/app/integration/onboarding/onboarding-done/onboarding-done.component.ts
@@ -27,8 +27,6 @@ export class OnboardingDoneComponent implements OnInit {
 
   navigateToDashboard(): void {
     this.trackSessionTime();
-    this.trackingService.onClickEvent(ClickEvent.ONBOARDING_DONE);
-    this.refinerService.triggerSurvey(RefinerSurveyType.ONBOARDING_DONE);
     this.router.navigate([`/workspaces/main/dashboard`]);
   }
 
