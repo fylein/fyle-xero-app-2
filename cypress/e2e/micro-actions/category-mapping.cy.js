7bd233e5a9d7c6afa427ac65bc7967e5e58797db
anishfyle <anishkumar.s@fyle.in>
2022-12-20 12:15:08 +0530

    Fixes - 1

--- cypress/e2e/micro-actions/category-mapping.cy.js
+++ cypress/e2e/micro-actions/category-mapping.cy.js
@@ -7,20 +7,14 @@ describe('category mapping', () => {
     })
   
     it('in xero-app : check category mapping', () => {
-        cy.getElement('side-nav-bar')
-        cy.assertText('mappings-text','Mappings')
         cy.getElement('side-nav-bar-click').contains('Mappings').click()
         cy.getElement('employee-mapping-text').contains('Category Mapping').click()
-        cy.getElement('arrow-back')
-        cy.get('.mapping-header-section--card').contains('Unmapped Categories').click()
+        cy.get('.mapping-header-section--card').contains('Unmapped Categories')
         cy.getElement('mapping-card').contains('Total Categories').click()
-        cy.getElement('mapping-filter')
-        cy.getElement('search-svg')
         cy.getElement('search-element').type('bill')
         cy.getElement('clear-icon-svg').click()
         cy.getElement('search-element').type('invalid')
         cy.getElement('zero-state-image')
-        cy.getElement('clear-icon-svg').click
         cy.getElement('clear-icon-svg').click()
         cy.get('.mat-header-cell').contains('Category in Fyle')
         cy.getElement('table-header-text').contains('Account in Xero')
