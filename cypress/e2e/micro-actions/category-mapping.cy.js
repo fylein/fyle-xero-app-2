9d80969f92df6a35d73c6adb2b9e4b572e5c0ef7
Anish Kr Singh <116036738+anishfyle@users.noreply.github.com>
2022-12-20 12:34:03 +0530

    category-mapping E2E Test (#50)
    * category-mapping E2E Test
    * Removed slack video update

--- cypress/e2e/micro-actions/category-mapping.cy.js
+++ cypress/e2e/micro-actions/category-mapping.cy.js
@@ -1,28 +1,23 @@
 /// <reference types="cypress" />
 
-describe('employee mapping', () => {
+describe('category mapping', () => {
     beforeEach(() => {
       cy.microActionLogin()
       cy.visit('/')
     })
   
-    it('in xero-app : check employee mapping', () => {
-        cy.getElement('side-nav-bar')
-        cy.assertText('mappings-text','Mappings')
+    it('in xero-app : check category mapping', () => {
         cy.getElement('side-nav-bar-click').contains('Mappings').click()
-        cy.getElement('employee-mapping-text').contains('Employee Mapping').click()
-        cy.getElement('arrow-back')
-        cy.get('.mapping-header-section--card').contains('Unmapped Employees').click()
-        cy.getElement('mapping-card').contains('Total Employees').click()
-        cy.getElement('mapping-filter')
-        cy.getElement('search-svg')
-        cy.getElement('search-element').type('admin')
+        cy.getElement('employee-mapping-text').contains('Category Mapping').click()
+        cy.get('.mapping-header-section--card').contains('Unmapped Categories')
+        cy.getElement('mapping-card').contains('Total Categories').click()
+        cy.getElement('search-element').type('bill')
         cy.getElement('clear-icon-svg').click()
         cy.getElement('search-element').type('invalid')
         cy.getElement('zero-state-image')
         cy.getElement('clear-icon-svg').click()
-        cy.get('.mat-header-cell').contains('Employee in Fyle')
-        cy.getElement('table-header-text').contains('Contact in Xero')
+        cy.get('.mat-header-cell').contains('Category in Fyle')
+        cy.getElement('table-header-text').contains('Account in Xero')
         cy.getElement('state-text').contains('State')
         cy.get('.mapping-table--state-pill-text').eq(0).then(($ele) => {
           expect($ele.text().trim()).to.be.oneOf(['Unmapped', 'Mapped'])
