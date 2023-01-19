047f357a44276f23c76402bfc20dd71f1dbb0934
Vishal Pandey <vishalpandeyviptsk@gmail.com>
2023-01-19 21:02:27 +0530

    added spaces and removed question mark

--- src/app/shared/components/configuration/export-settings/export-settings.component.ts
+++ src/app/shared/components/configuration/export-settings/export-settings.component.ts
@@ -274,14 +274,14 @@ export class ExportSettingsComponent implements OnInit, OnDestroy {
       this.mappingService.getGroupedXeroDestinationAttributes(destinationAttributes)
     ];
 
-    if (!this.isOnboarding){
+    if (!this.isOnboarding) {
       getSettingsData.push(this.workspaceService.getWorkspaceGeneralSettings());
     }
 
     forkJoin(getSettingsData).subscribe(response => {
       this.exportSettings = response[0] as ExportSettingGet;
       this.bankAccounts = (response[1] as GroupedDestinationAttribute).BANK_ACCOUNT;
-      if (!this.isOnboarding){
+      if (!this.isOnboarding) {
         this.is_simplify_report_closure_enabled = (response[2] as WorkspaceGeneralSetting).is_simplify_report_closure_enabled;
       }
       this.cccExpenseStateOptions = [
