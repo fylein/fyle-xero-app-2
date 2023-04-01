8d627e2cb3f831f5da54724c4f18b4bf963a9d8f
Anish Kr Singh <116036738+anishfyle@users.noreply.github.com>
2023-04-02 04:41:39 +0530

    Export log e2e test (#65)
    * export-log-e2e-test

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -6,6 +6,8 @@ declare global {
   namespace Cypress {
     interface Chainable {
       microActionLogin(): void;
+      journeyLogin(): void;
+      selectMatOption(optionName: string): void;
       getElement(attributeName: string): Cypress.Chainable<JQuery<HTMLElement>>;
       assertText(attributeName: string, text: string): void;
       setupHttpListeners(): void;
@@ -25,6 +27,10 @@ Cypress.Commands.add('setupHttpListeners', () => {
   setupInterceptor('GET', '/api/partner/orgs/', 'OrgDetails');
 });
 
+Cypress.Commands.add('selectMatOption', (optionName) => {
+  cy.get('mat-option').contains(optionName).click();
+});
+
 Cypress.Commands.add('assertText', (attributeName: string, text: string) => {
   cy.getElement(attributeName).should('include.text', text)
 })
@@ -41,9 +47,27 @@ Cypress.Commands.add('microActionLogin', () => {
     full_name: "Anish",
     user_id: "xyz",
     org_id: environment.e2e_tests.secret[0].org_id,
-    env:environment.e2e_tests.env,
+    env: environment.e2e_tests.env,
     org_name: "XYZ Org"
   };
   window.localStorage.setItem('user', JSON.stringify(user))
   cy.setupHttpListeners();
 })
+
+Cypress.Commands.add('journeyLogin', () => {
+  const user = {
+    email: 'admin1@fyleforfyleXeroJourney.com',
+    refresh_token: environment.e2e_tests.secret[1].refresh_token,
+    access_token: environment.e2e_tests.secret[1].access_token,
+    full_name: "Anish",
+    user_id: "xyz",
+    org_id: environment.e2e_tests.secret[1].org_id,
+    env:environment.e2e_tests.env,
+    org_name: "XYZ Org"
+  };
+  window.localStorage.setItem('user', JSON.stringify(user));
+  window.localStorage.setItem('workspaceId', JSON.stringify(environment.e2e_tests.secret[1].workspace_id));
+  window.localStorage.setItem('access_token', JSON.stringify(user.access_token));
+  window.localStorage.setItem('refresh_token', JSON.stringify(user.refresh_token));
+  cy.setupHttpListeners();
+});
